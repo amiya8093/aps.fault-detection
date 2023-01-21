4ae73c5ab17871949d601cae29646f93496cc9b6
amiya <amiyaprakash8093@gmail.com>
2023-01-21 06:53:24 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirement():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.2",
+    author="amiya_prakash",
+    author_email="amiyaprakash8093@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
